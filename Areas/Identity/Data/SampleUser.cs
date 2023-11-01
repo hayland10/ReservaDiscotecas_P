1f165a68c77f931830f8b0e64a6f59c6835ccf0c
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2023-10-31 22:01:58 -0500

    Agregar archivos de proyecto.

---
+++ Areas/Identity/Data/SampleUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ReservaDiscotecas_P.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the SampleUser class
+public class SampleUser : IdentityUser
+{
+    
+    public string Name { get; set; }
+    public string Telefono { get; set; }    
+
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReservaDiscotecas_P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
